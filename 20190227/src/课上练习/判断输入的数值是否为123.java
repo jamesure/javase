f4b4170b43ca20511c4504086b61261fd4472dd0
jamesure <840601002@qq.com>
2019-02-27 11:12:25 +0800

    20190227

--- 20190227/src/课上练习/判断输入的数值是否为123.java
+++ 20190227/src/课上练习/判断输入的数值是否为123.java
@@ -3,22 +3,30 @@ import java.util.*;
 public class 判断输入的数值是否为123 {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int a;
-        do {
-            System.out.println("请选择功能");
-            a=in.nextInt();
-        } while (a!=1&&a!=2&&a!=3);
-            switch (a){
-                case 1:
-                    System.out.println("登录");
-                    break;
-                case 2:
-                    System.out.println("注册");
-                    break;
-                case 3:
-                    System.out.println("再见");
-                    break;
-            }
 
+            System.out.println("************************");
+            System.out.println("\t\t"+"1：登录");
+            System.out.println("\t\t"+"2：注册");
+            System.out.println("\t\t"+"3：退出");
+            System.out.println("************************");
+        while (true){
+            System.out.println("输入命令");
+            int a=in.nextInt();
+            while (!(a>=1&&a<=3)){
+                System.out.println("请重新输入命令");
+                a=in.nextInt();
+            }
+            if (a==1){
+                System.out.println("登录");
+            }
+            if (a==2){
+                System.out.println("注册");
+            }
+            if (a==3){
+                System.out.println("退出");
+                System.out.println("B y e");
+                break;
+            }
+        }
     }
 }
